742faa560c61de0c7a003b2487335624b215d24a
Franck BENAULT <franck.benault@atos.net>
2017-05-26 00:32:55 +0200

    add log in spring boot application

--- 10SpringBootExample/11springexample/src/main/java/hello/HelloController.java
+++ 10SpringBootExample/11springexample/src/main/java/hello/HelloController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+	
+	private org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(HelloController.class);
 
     @RequestMapping("/")
     public String index() {
+    	logger.info("main page");
+    	
         return "Greetings from Spring Boot!";
     }
 
