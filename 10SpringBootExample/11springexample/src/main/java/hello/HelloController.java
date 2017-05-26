d4db5d8cdc8734532023378522777b1e30667083
Franck BENAULT <franck.benault@atos.net>
2017-05-26 16:09:07 +0200

    add random error 5 pc

--- 10SpringBootExample/11springexample/src/main/java/hello/HelloController.java
+++ 10SpringBootExample/11springexample/src/main/java/hello/HelloController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +10,17 @@ public class HelloController {
 	
 	private org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(HelloController.class);
 
+
     @RequestMapping("/")
     public String index() {
     	logger.info("main page");
+    	
+    	Random rn = new Random();
+    	int answer = rn.nextInt(100) + 1;
+    	if(answer>95) {
+    		logger.error("main page http 404");
+    		throw new ResourceNotFoundException();
+    	}
     	
         return "Greetings from Spring Boot!";
     }
