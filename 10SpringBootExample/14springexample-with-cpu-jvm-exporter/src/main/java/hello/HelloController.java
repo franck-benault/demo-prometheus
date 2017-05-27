1e660494c2fbdb4261db5f105fdaa8c5cb8f8f08
Franck BENAULT <franck.benault@atos.net>
2017-05-27 22:51:09 +0200

    add example 04

---
+++ 10SpringBootExample/14springexample-with-cpu-jvm-exporter/src/main/java/hello/HelloController.java
@@ -0,0 +1,34 @@
+package hello;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+	
+	private org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(HelloController.class);
+
+
+    @RequestMapping("/")
+    public String index() {
+    	logger.info("main page");
+    	
+    	Random rn = new Random();
+    	int answer = rn.nextInt(100) + 1;
+    	if(answer>97) {
+    		logger.error("main page http 404");
+    		throw new ResourceNotFoundException();
+    	}
+    	
+    	List<String> list  = new ArrayList<String>();
+    	for(int i=0; i<100000; i++)
+    		list.add("eeeeeeeeeeeeeeeeeee"+i);
+    	
+        return "Greetings from Spring Boot!";
+    }
+
+}
