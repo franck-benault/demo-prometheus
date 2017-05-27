dd36c36e2f315c947a7a5c7c87bb83d98cbc3157
Franck BENAULT <franck.benault@atos.net>
2017-05-27 14:55:18 +0200

    project example12 add logging

--- 10SpringBootExample/12springexample02-with-springboot-end-points/src/main/java/hello/HelloController.java
+++ 10SpringBootExample/12springexample02-with-springboot-end-points/src/main/java/hello/HelloController.java
@@ -1,13 +1,27 @@
 package hello;
 
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+	
+	private org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(HelloController.class);
+
 
     @RequestMapping("/")
     public String index() {
+    	logger.info("main page");
+    	
+    	Random rn = new Random();
+    	int answer = rn.nextInt(100) + 1;
+    	if(answer>97) {
+    		logger.error("main page http 404");
+    		throw new ResourceNotFoundException();
+    	}
+    	
         return "Greetings from Spring Boot!";
     }
 
