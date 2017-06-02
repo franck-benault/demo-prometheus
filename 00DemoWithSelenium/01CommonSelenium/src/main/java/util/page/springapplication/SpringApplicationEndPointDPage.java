d56352bb28ef3d464c69c3ef3b684c80d03a8a34
Franck BENAULT <franck.benault@atos.net>
2017-06-02 23:20:34 +0200

    complete example10 add graph for prometheus

---
+++ 00DemoWithSelenium/01CommonSelenium/src/main/java/util/page/springapplication/SpringApplicationEndPointDPage.java
@@ -0,0 +1,31 @@
+package util.page.springapplication;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+
+import util.page.Page;
+
+public class SpringApplicationEndPointDPage extends Page {
+	
+	private static final String url = "http://localhost:8080/endpointD";
+
+	public SpringApplicationEndPointDPage(WebDriver driver) {
+		super(driver);
+		driver.get(url);
+		
+		int nbTries =1;
+		//springboot
+		while(driver.getPageSource().contains("404") && nbTries<4) {
+			try {
+				TimeUnit.SECONDS.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			driver.get(url);
+			nbTries++;
+		}
+	}
+
+
+}
