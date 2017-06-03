1960661717c56a481bd6ef5c4a0d8edde0452cd4
Franck BENAULT <franck.benault@atos.net>
2017-06-03 10:45:38 +0200

    int example 11 and fix issue in example2

---
+++ 10SpringBootExample/21springexample11-with-springboot-end-points-dropwizzard/src/main/java/hello/HelloController.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+	
+	private org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(HelloController.class);
+
+
+    @RequestMapping("/")
+    public String index() {
+    	
+    	Random rn = new Random();
+    	int answer = rn.nextInt(100) + 1;
+    	if(answer>97) {
+    		logger.error("main page http 404");
+    		throw new ResourceNotFoundException();
+    	}
+    	
+    	logger.info("main page OK");
+        return "Greetings from Spring Boot!";
+    }
+
+}
