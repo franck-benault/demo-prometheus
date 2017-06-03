896c8d8c0e2ae47b2c475e51355fdd757b033915
Franck BENAULT <franck.benault@atos.net>
2017-06-03 10:03:48 +0200

    example 8 add quantile option

--- 10SpringBootExample/18springexample08-with-summary/src/main/java/hello/Metrics.java
+++ 10SpringBootExample/18springexample08-with-summary/src/main/java/hello/Metrics.java
@@ -17,6 +17,8 @@ public class Metrics {
 			.help("Number of temporary files open").register();
 	
 	public static final Summary responseTime = Summary.build()
+			.quantile(0.5, 0.05)
+			.quantile(0.9, 0.01)
 			.name("response_time_summary")
 			.labelNames("method")
 			.help("request response time summary").register();
