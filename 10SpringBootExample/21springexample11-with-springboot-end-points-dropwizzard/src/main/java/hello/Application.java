3756b4c03108f441e6418dc89819b0e6df5ff969
Franck BENAULT <franck.benault@atos.net>
2017-06-03 11:07:23 +0200

    complete example11

--- 10SpringBootExample/21springexample11-with-springboot-end-points-dropwizzard/src/main/java/hello/Application.java
+++ 10SpringBootExample/21springexample11-with-springboot-end-points-dropwizzard/src/main/java/hello/Application.java
@@ -8,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
+
 @SpringBootApplication
+@EnableMetrics(proxyTargetClass = true)
 public class Application {
 
     public static void main(String[] args) {
--- 10SpringBootExample/21springexample11-with-springboot-end-points-dropwizzard/src/main/java/hello/HelloController.java
+++ 10SpringBootExample/21springexample11-with-springboot-end-points-dropwizzard/src/main/java/hello/HelloController.java
@@ -5,12 +5,15 @@ import java.util.Random;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.codahale.metrics.annotation.Timed;
+
 @RestController
 public class HelloController {
 	
 	private org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(HelloController.class);
 
 
+    @Timed
     @RequestMapping("/")
     public String index() {
     	
