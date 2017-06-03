99e78f50e469771d81d7b2a583372b578b49d357
Franck BENAULT <franck.benault@atos.net>
2017-06-03 11:57:23 +0200

    example4 cleaning import

--- 10SpringBootExample/14springexample-with-cpu-jvm-exporter/src/main/java/hello/HelloController.java
+++ 10SpringBootExample/14springexample-with-cpu-jvm-exporter/src/main/java/hello/HelloController.java
@@ -2,8 +2,6 @@ package hello;
 
 import hello.util.Util;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import org.springframework.web.bind.annotation.RestController;
